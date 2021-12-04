db303b4e88d30c45f42b92232eb454a4142a2860
xstahol <staffan.holmgren@tre.se>
2021-12-04 16:09:22 +0100

    scala aoc 2021. Day4 part two

--- src/main/scala/D4.scala
+++ src/main/scala/D4.scala
@@ -1,48 +1,101 @@
+import scala.collection.{mutable}
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
 import scala.util.control.Breaks
-import scala.util.control.Breaks.break
 
 object D4 {
   def main(args: Array[String]): Unit = {
     val input: Seq[String] = Source.fromFile("input/d4.txt").mkString.split("\n\n").toList
-    val bingoNumbers: List[String] = input.head.split(",").toList;
-    val bingoBoards: Seq[Array[Array[Point]]] = input.tail.map(parseBingoBoard)
+    val bingoNumbers: List[String] = input.head.split(",").toList
+    //val bingoBoards: Seq[Array[Array[Point]]] = input.tail.map(parseBingoBoard)
+    val bingoBoardsPartTwo: Seq[Array[Array[Point]]] = input.tail.map(parseBingoBoard)
 
-    partOne(bingoNumbers, bingoBoards)
-
+    //partOne(bingoNumbers, ListBuffer.empty ++= bingoBoards)
+    partTwo(bingoNumbers, ListBuffer.empty ++= bingoBoardsPartTwo)
   }
 
-  def partOne(bingoNumbers: List[String], bingoBoards: Seq[Array[Array[Point]]]): Unit = {
+  def partTwo(bingoNumbers: List[String], bingoBoards: ListBuffer[Array[Array[Point]]]): Unit = {
+    var lastBingoBoard: Array[Array[Point]] = Array.empty
+    var lastBingoNumber = ""
     val loop = new Breaks
     loop.breakable {
       for (number <- bingoNumbers) {
         markNumberInBingoBoards(number, bingoBoards)
-        val bingoBoard = isBingo(bingoBoards)
+        val boardsWithBingo = isBingoSupportingMultipleBingo(bingoBoards)
+        if (boardsWithBingo.isDefined) {
+          boardsWithBingo.get.foreach(b => {
+            bingoBoards.remove(b._2)
+            lastBingoNumber = number
+            lastBingoBoard = b._1
+            println(calcUnmarkedNumbersSum(lastBingoBoard) * lastBingoNumber.toInt)
+          }
+          )
+        }
+      }
+    }
+    println(calcUnmarkedNumbersSum(lastBingoBoard) * lastBingoNumber.toInt)
+
+  }
+
+  def partOne(bingoNumbers: List[String], bingoBoards: mutable.Seq[Array[Array[Point]]]): Unit = {
+    val loop = new Breaks
+    loop.breakable {
+      for (number <- bingoNumbers) {
+        markNumberInBingoBoards(number, bingoBoards)
+        val (bingoBoard, idx) = isBingo(bingoBoards)
         if (bingoBoard.isDefined) {
-          println(calcUnmarkedNumbersSum(bingoBoard.get) * number.toInt)
+          println("part one: " + calcUnmarkedNumbersSum(bingoBoard.get) * number.toInt)
           loop.break()
         }
       }
     }
   }
 
-  def isBingo(bingoBoards: Seq[Array[Array[Point]]]): Option[Array[Array[Point]]]= {
-    for (board <- bingoBoards) {
+  def isBingo(bingoBoards: mutable.Seq[Array[Array[Point]]]): (Option[Array[Array[Point]]], Int) = {
+
+    for ((board, idx) <- bingoBoards.zipWithIndex) {
       for (row <- board) {
         // check rows
         if (row.forall(_.chosen)) {
-          return Some(board)
+          return (Some(board), idx)
         }
       }
 
       // check columns by using transpose so columns gets as rows
       for (col <- board.transpose) {
         if (col.forall(_.chosen)) {
-          return Some(board)
+          return (Some(board), idx)
         }
       }
     }
-    None
+    (None, -1)
+  }
+
+  def isBingoSupportingMultipleBingo(bingoBoards: mutable.Seq[Array[Array[Point]]]): Option[ListBuffer[(Array[Array[Point]], Int)]] = {
+    var boardsGotBingo: ListBuffer[Tuple2[Array[Array[Point]], Int]] = ListBuffer.empty
+    for ((board, idx) <- bingoBoards.zipWithIndex) {
+      var gotBingo = false
+      for (row <- board) {
+        // check rows
+        if (row.forall(_.chosen)) {
+          boardsGotBingo.prepend((board, idx))
+          gotBingo = true
+        }
+      }
+      if (!gotBingo) {
+        // check columns by using transpose so columns gets as rows
+        for (col <- board.transpose) {
+          if (col.forall(_.chosen)) {
+            boardsGotBingo.prepend((board, idx))
+          }
+        }
+      }
+    }
+
+    if (boardsGotBingo.nonEmpty)
+      Some(boardsGotBingo)
+    else
+      None
   }
 
   def calcUnmarkedNumbersSum(board: Array[Array[Point]]): Int = {
@@ -55,7 +108,7 @@ object D4 {
     sum
   }
 
-  def markNumberInBingoBoards(number: String, bingoBoards: Seq[Array[Array[Point]]]): Unit = {
+  def markNumberInBingoBoards(number: String, bingoBoards: mutable.Seq[Array[Array[Point]]]): Unit = {
     for (board <- bingoBoards; row <- board; point <- row) {
       if (point.value == number) {
         point.chosen = true
