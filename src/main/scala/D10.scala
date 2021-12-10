53101580b16c55508938ab0ed04f66dee521703a
xstahol <staffan.holmgren@tre.se>
2021-12-10 10:31:06 +0100

    day 10 part two. had to use big int

--- src/main/scala/D10.scala
+++ src/main/scala/D10.scala
@@ -4,14 +4,40 @@ import scala.io.Source
 object D10 {
   def main(args: Array[String]): Unit = {
     val input: Seq[String] = Source.fromFile("input/d10.txt").getLines().toList
-    val completeInstructions = input
-    val sum = completeInstructions
+    one(input)
+    two(input)
+  }
+
+  def one(input: Seq[String]) = {
+    val sum = input
       .map(findIllegalInstruction)
       .filter(_.isDefined)
       .map(illegalInstruction => calculateIllegalCharScore(illegalInstruction.get))
       .sum
 
     println(sum)
+  }
+
+  def two(input: Seq[String]) = {
+    val sorted = input
+      .filter(instruction => findIllegalInstruction(instruction).isEmpty)
+      .map(closeUncompletedInstruction)
+      .map(paddedInstructions => {
+        var tmpSum: BigInt = 0
+        val scores = paddedInstructions.map {
+          case ")" => 1
+          case "]" => 2
+          case "}" => 3
+          case ">" => 4
+        }
+        scores.foreach { score =>
+          tmpSum *= 5
+          tmpSum += score
+        }
+        tmpSum
+      })
+      .sorted
+    println(sorted(sorted.length / 2))
   }
 
   def findIllegalInstruction(instruction: String): Option[String] = {
@@ -34,10 +60,23 @@ object D10 {
       } else {
         openChunks.push(curr)
       }
-
-
     }
     None
+  }
+
+  def closeUncompletedInstruction(instruction: String): List[String] = {
+    val openChunks: mutable.Stack[String] = new mutable.Stack[String]()
+
+    for (i <- 0 until instruction.length) {
+      val curr = instruction.charAt(i).toString
+      if (isCloseInstruction(curr)) {
+        openChunks.pop()
+      } else {
+        openChunks.push(curr)
+      }
+    }
+
+    openChunks.map(closingStr).toList
   }
 
   def isCloseInstruction(instruction: String): Boolean = {
@@ -59,8 +98,8 @@ object D10 {
     }
   }
 
-  def calculateIllegalCharScore(illegarChar: String): Int = {
-    illegarChar match {
+  def calculateIllegalCharScore(illegalChar: String): Int = {
+    illegalChar match {
       case ")" => 3
       case "]" => 57
       case "}" => 1197
